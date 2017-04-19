3ba622c61e65b148dd428133c1ef8fb791284827
Mira Sarkar <anoop.sarkar@gmail.com>
2017-04-18 18:56:17 -0700

    removed variable names

--- python-tutorial/storytime.py
+++ python-tutorial/storytime.py
@@ -19,15 +19,9 @@ roles = ["seer", "wizard", "genius", "teacher", "editor", "cook", "gamekeeper", 
 actions = ["find", "get", "learn", "buy", "travel", "hide", "cook", "predict", "raise", "code"]  
 places = ["Green Palace", "Flying Academy", "Grinwald School of Magic", "Dungeon Kitchen", "Enchanted Computer Room", "Cleaner's Hut", "Grassy Grounds"]
 
-actor_name = random.choice(names)
-actor_role = random.choice(roles)
-quest = random.choice(actions)
-start_place = random.choice(places)
-end_place = random.choice(places)
-
-story = "In the " + start_place + " there was a " + \
-        actor_role + " called " + actor_name + \
-        " who did not want to go to the " + end_place + "."
+story = "In the " + random.choice(places) + " there was a " + \
+        random.choice(roles) + " called " + random.choice(names) + \
+        " who did not want to go to the " + random.choice(places) + "."
 print(story)
 
         
