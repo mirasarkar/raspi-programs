c3f2fb7c9a03ddf24c99619f08bee7c206e31bf7
Mira Sarkar <anoop.sarkar@gmail.com>
2017-04-18 19:30:37 -0700

    building has two floors now

--- minecraft/building.py
+++ minecraft/building.py
@@ -3,14 +3,14 @@ import time
 mc = Minecraft.create()
 
 # where to build the house
-x=35
-y=0
-z=-56
-
+x=-95
+y=9.0
+z=74.8
+ 
 # size of the house
-width=3
-height=3
-length=3
+width=5
+height=4
+length=7
 
 # different types of blocks
 ice=79
@@ -21,7 +21,9 @@ stone=1
 # use setBlocks to make a building
 mc.setBlocks(x,y,z,x+width,y+height,z+length,stone)
 #mc.setBlocks(x,y,z,x+width,y+height,z+length,ice)
-mc.setBlocks(x+1,y,z+1,x+width-1,y+height,z+length-1,water)
+mc.setBlocks(x+1,y,z+1,x+width-1,y+height-1,z+length-1,air)
 
 #time.sleep(20)
 #mc.setBlocks(x,y,z,x+width,y+height,z+length,air)
+mc.setBlocks(x,y+height,z,x+width,y+height*2,z+length,stone)
+mc.setBlocks(x+1,y+height+1,z+1,x+width-1,y+height*2-1,z+length-1,air)
