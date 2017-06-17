a38b1cacaf0ac426a4bc2631ba99686afc28184b
Mira Sarkar <anoop.sarkar@gmail.com>
2017-06-16 17:01:20 -0700

    teleport code from minecraft book

---
+++ minecraft/teleport.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+# Mira's house is at -92, 9, 85
+
+x=-92
+y=9
+z=85
+
+mc.player.setTilePos(x,y,z)
+                                                  
