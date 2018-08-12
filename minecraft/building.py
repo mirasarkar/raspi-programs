83046dc8aac86a8ed6de4f36b00d2b42c60da7c4
Mira Sarkar <anoop.sarkar@gmail.com>
2018-08-12 16:02:58 -0700

    make building appear around where you are

--- minecraft/building.py
+++ minecraft/building.py
@@ -2,15 +2,18 @@ from mcpi.minecraft import Minecraft
 import time
 mc = Minecraft.create()
 
+# get the player position
+(player_x, player_y, player_z) = mc.player.getTilePos()
+
 # where to build the house
-x=-95
-y=9.0
-z=74.8
+x=player_x-5
+y=player_y
+z=player_z-5
  
 # size of the house
-width=5
-height=4
-length=7
+width=10
+height=10
+length=10
 
 # different types of blocks
 ice=79
@@ -20,10 +23,15 @@ stone=1
 
 # use setBlocks to make a building
 mc.setBlocks(x,y,z,x+width,y+height,z+length,stone)
-#mc.setBlocks(x,y,z,x+width,y+height,z+length,ice)
 mc.setBlocks(x+1,y,z+1,x+width-1,y+height-1,z+length-1,air)
 
+time.sleep(10)
+mc.setBlocks(x,y,z,x+width,y+height,z+length,air)
+
+
+#mc.setBlocks(x,y,z,x+width,y+height,z+length,ice)
 #time.sleep(20)
 #mc.setBlocks(x,y,z,x+width,y+height,z+length,air)
-mc.setBlocks(x,y+height,z,x+width,y+height*2,z+length,stone)
-mc.setBlocks(x+1,y+height+1,z+1,x+width-1,y+height*2-1,z+length-1,air)
+#mc.setBlocks(x,y+height,z,x+width,y+height*2,z+length,stone)
+#mc.setBlocks(x+1,y+height+1,z+1,x+width-1,y+height*2-1,z+length-1,air)
+
--- minecraft/tour.py
+++ minecraft/tour.py
@@ -1,14 +1,21 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 import time
-x=0
-y=0
-z=0
+
+x=45.2
+y=0.0
+z=95.4
+mc.player.setTilePos(x,y,z)
+time.sleep(2)
+
+x=7.3
+y=9.0
+z=65.8
 mc.player.setTilePos(x,y,z)
 time.sleep(10)
 
-x=54
-y=0
-z=18
+x=45.2
+y=0.0
+z=95.4
 mc.player.setTilePos(x,y,z)
 
